77fb3dfd8dfbc42a8f5012525dd6edd0123d36c5
David Wyman <davidmwyman@gmail.com>
2023-02-02 14:29:31 -0500

    add 15 cards, card stack

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+// array of cards = the card stack
+const cardStack = [
+  card_001_dog,
+  card_002_car,
+  card_003_cow,
+  card_004_banana,
+  card_005_apple,
+  card_006_table,
+  card_007_window,
+  card_008_clock,
+  card_009_ball,
+  card_010_book,
+  card_011_train,
+  card_012_plane,
+  card_013_ear,
+  card_014_nose,
+  card_015_mouth,
+]
 
+// current card template
+// const currentCard =
---
+++ cards.js
@@ -0,0 +1,96 @@
+// individual cards as objects with 3 properties: word, image, ID
+const card_001_dog = {
+  word: `Dog`,
+  // TODO: ADD image property and find an image
+  ID: 001,
+}
+
+const card_002_car = {
+  word: `Car`,
+  // TODO: ADD image property and find an image
+  ID: 002,
+}
+
+const card_003_cow = {
+  word: `Cow`,
+  // TODO: ADD image property and find an image
+  ID: 003,
+}
+
+const card_004_banana = {
+  word: `Banana`,
+  // TODO: ADD image property and find an image
+  ID: 004,
+}
+
+const card_005_apple = {
+  word: `Apple`,
+  // TODO: ADD image property and find an image
+  ID: 005,
+}
+
+const card_006_table = {
+  word: `Table`,
+  // TODO: ADD image property and find an image
+  ID: 006,
+}
+
+const card_007_window = {
+  word: `Window`,
+  // TODO: ADD image property and find an image
+  ID: 007,
+}
+
+const card_008_clock = {
+  word: `Clock`,
+  // TODO: ADD image property and find an image
+  ID: 008,
+}
+
+const card_009_ball = {
+  word: `Ball`,
+  // TODO: ADD image property and find an image
+  ID: 009,
+}
+
+const card_010_book = {
+  word: `Book`,
+  // TODO: ADD image property and find an image
+  ID: 010,
+}
+
+const card_011_train = {
+  word: `Train`,
+  // TODO: ADD image property and find an image
+  ID: 011,
+}
+
+const card_012_plane = {
+  word: `plane`,
+  // TODO: ADD image property and find an image
+  ID: 012,
+}
+
+const card_013_ear = {
+  word: `Ear`,
+  // TODO: ADD image property and find an image
+  ID: 013,
+}
+
+const card_014_nose = {
+  word: `Nose`,
+  // TODO: ADD image property and find an image
+  ID: 014,
+}
+
+const card_015_mouth = {
+  word: `Mouth`,
+  // TODO: ADD image property and find an image
+  ID: 015,
+}
+
+console.log(card_007_window.word)
+console.log(card_008_clock.word)
+
+console.log(card_007_window.ID)
+console.log(card_008_clock.ID)
